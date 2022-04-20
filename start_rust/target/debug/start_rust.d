240a4fc0f8d3f173afd30cc219ca6009698dadd4
Alvo Rithm <alvorithmemail@gmail.com>
2022-04-20 09:53:56 +0200

    Initial commit - created hello_world program using cargo package manager - ran with cargo run instead of cargo build

---
+++ start_rust/target/debug/start_rust.d
@@ -0,0 +1,1 @@
+/home/alvorithm/Documents/FOCUS_SAULS/solidity_rust_fullstack/Rustlanging/start_rust/start_rust/target/debug/start_rust: /home/alvorithm/Documents/FOCUS_SAULS/solidity_rust_fullstack/Rustlanging/start_rust/start_rust/src/main.rs
---
+++ start_rust/target/rls/debug/deps/start_rust-2e97a80cee0501e3.d
@@ -0,0 +1,5 @@
+/home/alvorithm/Documents/FOCUS_SAULS/solidity_rust_fullstack/Rustlanging/start_rust/start_rust/target/rls/debug/deps/start_rust-2e97a80cee0501e3.rmeta: src/main.rs
+
+/home/alvorithm/Documents/FOCUS_SAULS/solidity_rust_fullstack/Rustlanging/start_rust/start_rust/target/rls/debug/deps/start_rust-2e97a80cee0501e3.d: src/main.rs
+
+src/main.rs:
