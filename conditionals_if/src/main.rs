4b43f943fb27d8f4865fbd24fd9c72a362dba097
Alvo Rithm <alvorithmemail@gmail.com>
2022-04-26 08:04:10 +0200

    created condtional_match program to illustrate functionality and syntax

--- conditionals_if/src/main.rs
+++ conditionals_if/src/main.rs
@@ -3,18 +3,19 @@
 
 fn main() {
     let marks: i32 = 75;
-
+    // D symbol if arm
     if marks >= 50 && marks <= 59 {
         println!("You passed and got a D symbol");
     }
+    // C symbol else if arm
     else if marks >= 60 && marks < 75 {
-        println!("You passed and got a D symbol");
+        println!("You passed and got a C symbol");
     }
-
+    // anything less than 50 percent is a fail but do not give up:-)
     else if marks < 50 {
         println!("Do not gve up - give it another try:-)");
     }
-
+    // else arm: all other condtions as in great than or equal to 75
     else {
         println!("Congratulations: you passed with a distinction");
     }
--- conditionals_match/src/main.rs
+++ conditionals_match/src/main.rs
@@ -1,3 +1,15 @@
+// match is like switch but it is an optimized version of switch
+
 fn main() {
-    println!("Hello, world!");
+    let marks: i32 = 49;
+ 
+    match marks {
+        50..=59 => println!("You passed and got a D symbol"),
+    // C symbol else if arm
+        60..=69 => println!("You passed and got a C symbol"),
+    // anything less than 50 percent is a fail but do not give up:-)
+        0..= 49 => println!("Did not pass, however do not gve up - give it another try:-)"),
+    // else arm: all other condtions as in great than or equal to 75
+        _ => println!("Congratulations: you passed with a distinction"),
+    }
 }
