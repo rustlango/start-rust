0d169e947dc544e8903491f35015b1b05cc8c873
Alvo Rithm <alvorithmemail@gmail.com>
2022-04-26 08:58:14 +0200

    rearranged match expressions or arms

--- conditionals_match/src/main.rs
+++ conditionals_match/src/main.rs
@@ -1,15 +1,16 @@
 // match is like switch but it is an optimized version of switch
 
 fn main() {
-    let marks: i32 = 82;
+    let marks: i32 = 88;
  
     match marks {
-        50..=59 => println!("You passed and got a D symbol"),
-    // C symbol else if arm
-        60..=69 => println!("You passed and got a C symbol"),
-    // anything less than 50 percent is a fail but do not give up:-)
+        // anything less than 50 percent is a fail but do not give up:-)
         0..= 49 => println!("Did not pass, however do not gve up - give it another try:-)"),
-    // else arm: all other condtions as in great than or equal to 75
+        // D symbol match
+        50..=59 => println!("You passed and got a D symbol"),
+        // C symbol match
+        60..=69 => println!("You passed and got a C symbol"),
+        // else arm: all other condtions as in great than or equal to 75
         _ => println!("Congratulations: you passed with a distinction"),
     }
 }
